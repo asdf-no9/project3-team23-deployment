ef25590d9913a8e42ef7a47aa259cf4060535d96
Elliot <elliotmichlin@gmail.com>
2025-04-02 22:00:57 -0500

    added category selection UI

--- frontend/src/pages/orderKiosk.jsx
+++ frontend/src/pages/orderKiosk.jsx
@@ -5,27 +5,48 @@ import { Link } from 'react-router';
 
 export default function OrderKiosk() {
 
-    const [text, setText] = useState("NOTHING!!!")
+    const [isLoading, setLoading] = useState(true);
+    const [categories, setCategories] = useState({});
+
+    const buttonArray = [];
 
     useEffect(() => {
         fetch("http://localhost:3000/menu")
             .then((response) => response.json())
             .then((r) => {
-                setText(JSON.stringify(r))
+                setLoading(false);
+                setCategories(r["categories"]);
+                console.log(r);
             })
             .catch((e) => {
                 console.error(e)
-                setText("ERROR!: " + e.message)
             })
     }, [])
 
+    for (let i in categories) {
+        console.log(categories[i]);
+        buttonArray.push(<button className='catbuttonitem'>{i}</button>)
+    }
+
     return (
         <div class="layout">
-            <div class="mainBody"><p>{text}</p></div>
+            <div class="mainBody">
+                <h2>Select Category</h2>
+                {isLoading ?
+                    <p className='centeralign'>Loading...</p> :
+                    <div className='catbuttons'>{buttonArray}</div>
+                }
+            </div>
             <div class="subtotal">
                 <div className='itemlist'>
-
+                    <h3 className='centeralign'>Current Order</h3>
+                    <hr />
+                    <ol>
+                        <li>Test</li>
+                        <li>Test 2</li>
+                    </ol>
                 </div>
+                <button className='totalButton'>Total: $0.00</button>
             </div>
         </div>
     )
