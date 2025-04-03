1ed26c0044bb228f30d67e00dc53c1ef1a4a5a04
Antony <ant2022@tamu.edu>
2025-04-02 20:00:27 -0500

    Sidebar button and logo layout

--- frontend/src/components/sidebar.jsx
+++ frontend/src/components/sidebar.jsx
@@ -1,7 +1,27 @@
+import '../styles/layout.css';
+import logo from '../assets/ShareTeaLogo.png';
+
 export default function Sidebar() {
     return (
         <div>
-            <p>SIDEBAR</p>
+            <div className="sidebarContent">
+                <div className="logoContainer">
+                    <img src={logo} alt="ShareTea Logo" className="logo" />
+                </div>
+                <div className = "buttonContainer">
+                    <button className="drinks">Drinks</button>
+                    <button className="iceCream">Ice Cream</button>
+                    <button className="food">Food</button>
+                    <button className="specialItems">Special Items</button>
+                </div>
+
+                <div className = "accessibleFeatures">
+                    <button className="language">Language</button>
+                    <button className="highContrast">High-Contrast Theme</button>
+                    <button className="food">Food</button>
+                    <button className="specialItems">Special Items</button>
+                </div>
+            </div>
         </div>
     )
 }
