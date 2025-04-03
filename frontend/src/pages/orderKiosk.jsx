edf096f7fc229c8891e0d3415cc27ae0047bf439
Brayden Bailey <brayden.bailey@tamu.edu>
2025-04-02 20:48:38 -0500

    fixed class to className error

--- frontend/src/pages/orderKiosk.jsx
+++ frontend/src/pages/orderKiosk.jsx
@@ -20,9 +20,9 @@ export default function OrderKiosk() {
     }, [])
 
     return (
-        <div class="layout">
-            <div class="mainBody"><p>{text}</p></div>
-            <div class="subtotal"> </div>
+        <div className="layout">
+            <div className="mainBody"><p>{text}</p></div>
+            <div className="subtotal"> </div>
         </div>
     )
 }
