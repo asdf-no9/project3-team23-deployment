f99ef4f0e32027d47578aefe1ffc9fe61d45bb87
Brayden Bailey <brayden.bailey@tamu.edu>
2025-04-16 15:50:23 -0500

    added retrospective meeting notes

---
+++ frontend/src/pages/managerInventory.jsx
@@ -0,0 +1,11 @@
+
+import '../styles/layout.css';
+import { Link } from 'react-router';
+
+export default function managerInventory() {
+    return (
+        <div className="startBody">
+            <p className="startTitle"> Manager Inventory </p>
+        </div>
+    )
+}
