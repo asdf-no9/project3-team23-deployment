8f28721457cd58d57f72289bbbe66d57da8ad5e5
Brayden Bailey <brayden.bailey@tamu.edu>
2025-04-24 16:06:15 -0500

    added comments to manager inventory

--- frontend/src/pages/managerInventory.jsx
+++ frontend/src/pages/managerInventory.jsx
@@ -2,6 +2,14 @@
 import '../styles/layout.css';
 import { Link } from 'react-router';
 
+/**
+ * Used to display the manager inventory
+ * Will be used for adding, deleting, and editing inventory itesm
+ * Only accessible by managers
+ * @returns ManagerInventory component
+ * @author Brayden Bailey
+ */
+
 export default function managerInventory() {
     return (
         <div className="startBody">
